a7b95f24030b2e907f227785c338e101df611ce2
Corbin Taylor <cjtaylor@astro.umd.edu>
2019-10-04 01:57:53 -0400

    Working more on weather app. Index.js now renders Weather rather than App. Weather can fetch weather data using utils/GetWeather. I temporarily moved Weather.js out of the directory to do some cleaning.

---
+++ src/utils/GetWeather.js
@@ -0,0 +1,26 @@
+import Keys from './api_key.js'
+
+const getWeather = {
+	async fetchWeather(place) {
+		let endpoint = 'http://api.weatherstack.com/current?access_key=' + Keys.weather_key + '&query=' + place
+
+		try {
+			let response = await fetch(endpoint);
+			if (response.ok){
+				let data = await response.json();
+				return {
+					location: data.location.name,
+					time: data.location.localtime,
+					temperature: data.current.temperature,
+					icon: data.current.weather_icons[0],
+				};
+			} else {
+				throw new Error('Request failed!');
+			}
+		}catch(error){
+			console.log(error);
+		}
+	}
+};
+
+export default getWeather
