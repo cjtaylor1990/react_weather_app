f4b290d575636853c96ac5e3669f85877c91c891
Corbin Taylor <cjtaylor@astro.umd.edu>
2019-10-20 17:29:04 -0400

    Adding title bar to the Forecast component. I also changed the CSS of the Forecast and SearchBar to be more aesthetically pleasing.

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -6,13 +6,18 @@ class Forecast extends React.Component {
 	render() {
 		return (
 			<div className="Forecast">
-				<div className="Forecast-picture">
-					<img src={this.props.forecast.icon} alt={this.props.location}/>
+				<div className="Forecast-title">
+					<h1> My Weather App </h1>
 				</div>
-				<div className="Forecast-info">
-					<p>{this.props.forecast.location}</p>
-					<p>{this.props.forecast.temperature}</p>
-					<p>{this.props.forecast.time}</p>
+				<div className="Forecast-body">
+					<div className="Forecast-picture">
+						<img src={this.props.forecast.icon} alt={this.props.location}/>
+					</div>
+					<div className="Forecast-info">
+						<p>{this.props.forecast.location}</p>
+						<p>{this.props.forecast.temperature}</p>
+						<p>{this.props.forecast.time}</p>
+					</div>
 				</div>
 			</div>
 		);
